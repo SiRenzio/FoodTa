77c53da4c9fdb6aa82788f6776253cee81940ea1
victorjomnipre <soritavictorjom@gmail.com>
2024-11-02 23:41:24 +0800

    log in background

--- html/login_page.php
+++ html/login_page.php
@@ -7,22 +7,22 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
-    <div class="container">
-        <div class="wrapper">
-            <form action="index.php?command=order" method="post">
-                <h1>Login</h1>
-                <div class="input-field">
-                    <input type="text" placeholder="Username" name="user" autocomplete="off" required>
-                </div>
-                <div class="input-field">
-                    <input type="password" placeholder="Password" name="pass" autocomplete="off" required>
-                </div>
-                <button class="btn">Login</button>
-                <div class="register">
-                    <p>Don't have an account? <a href="#">Register</a></p>
-                </div>
-            </form>
-        </div>
-    </div>
+    <section class="logInPage">
+            <div class="wrapper">
+                <form action="index.php?command=order" method="post">
+                    <h1>Login</h1>
+                    <div class="input-field">
+                        <input type="text" placeholder="Username" name="user" autocomplete="off" required>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" placeholder="Password" name="pass" autocomplete="off" required>
+                    </div>
+                    <button class="btn">Login</button>
+                    <div class="register">
+                        <p>Don't have an account? <a href="#">Register</a></p>
+                    </div>
+                </form>
+            </div>
+    </section>
 </body>
 </html>
