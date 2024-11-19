4258d80f9e29801cc005d1dd7c127efaca879b74
victorjomnipre <soritavictorjom@gmail.com>
2024-11-19 10:09:40 +0800

    register design

--- html/register_page.php
+++ html/register_page.php
@@ -4,40 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/register_page.css">
+    <title>Food Ta! Delivery</title>
 </head>
 <body>
-    <div id = "selection">
-        <button id = "customerBtn" onclick="displayCustomerForm()">Order food!</button>
-        <button id = "storeBtn" onclick="displayStoreForm()">Sell food!</button>
+    
+    <div id="selection">
+        <img class="ftSecLogo" src="images/foodTaSectionLogo(Green).png" alt="Food Ta Logo">
+        <h2 class="brandline">Welcome To Food Ta! Delivery</h2>
+        <p class="tagline">Be One of Us.</p>
+        <div id="buttonContainer">
+            <button id="customerBtn" onclick="displayCustomerForm()">
+                <img src="images/customer.png" alt="Order Food" class="btnImg">Order food!
+            </button>
+            <button id="storeBtn" onclick="displayStoreForm()">
+                <img src="images/sell.png" alt="Sell Food" class="btnImg">Sell food!
+            </button>
+        </div>
+        <div id="customerRegister">
+            <form action="index.php?command=checkRegister&&accType=customer" method="POST" id="customerForm">
+                <h1 id="formHeader">Ready to order food?</h1>
 
-        <div id = "customerRegister">
-            <form action = 'index.php?command=checkRegister&&accType=customer' method = "POST" id = "customerForm">
-                <h1 id = "formHeader">Ready to order food?</h1>
+                <input type="text" placeholder="Full Name" name="fullname" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Username" name="user" class="formTxt" autocomplete="off" required>
+                <input type="password" placeholder="Password" name="pass" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Location" name="loc" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Contact No." name="contact" class="formTxt" autocomplete="off" required>
 
-                <input type="text" placeholder="Full Name" name="fullname" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Username" name="user" class = "formTxt" autocomplete="off" required>
-                <input type="password" placeholder="Password" name="pass" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Location" name="loc" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Contact No." name="contact" class = "formTxt" autocomplete="off" required>
-
-                <input type="submit" value = "Sign up" class = "formBtn">
+                <input type="submit" value="Sign up" class="formBtn">
+                <button type="button" class="backBtn" onclick="goBack()">Back</button>
             </form>
         </div>
         
-        <div id = "storeRegister">
-            <form action = 'index.php?command=checkRegister&&accType=store' method = "POST" id = "storeForm">
-                <h1 id = "formHeader">Ready to sell food?</h1>
+        <div id="storeRegister">
+            <form action="index.php?command=checkRegister&&accType=store" method="POST" id="storeForm">
+                <h1 id="formHeader">Ready to sell food?</h1>
 
-                <input type="text" placeholder="Full Name" name="fullname" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Store Name" name="storename" class = "formTxt" autocomplete="off" required>
-                <input type="password" placeholder="Password" name="pass" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Location" name="loc" class = "formTxt" autocomplete="off" required>
-                <input type="text" placeholder="Contact No." name="contact" class = "formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Full Name" name="fullname" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Store Name" name="storename" class="formTxt" autocomplete="off" required>
+                <input type="password" placeholder="Password" name="pass" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Location" name="loc" class="formTxt" autocomplete="off" required>
+                <input type="text" placeholder="Contact No." name="contact" class="formTxt" autocomplete="off" required>
                 
-                <input type="submit" value = "Sign up" class = "formBtn">
+                <input type="submit" value="Sign up" class="formBtn">
+                <button type="button" id="backBtn" class="backBtn" onclick="goBack()">Back</button>
             </form>
         </div>
     </div>  
-    <script src ='js/index.js'></script>
+    <script src="js/index.js"></script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
 const storeForm = document.getElementById('storeRegister');
 const buttons = document.getElementsByTagName('button');
 const customerForm = document.getElementById('customerRegister');
+const bckButton = document.getElementById('backBtn');
 
 function displayStoreForm(){
     storeForm.style.display = 'flex';
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.display = "none";
+        bckButton[i].style.display = "block";
     }
+
 }
 
 function displayCustomerForm(){
     customerForm.style.display = 'flex';
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].style.display = "none";
+        bckButton[i].style.display = "block";
+    }
+}
+
+function goBack() {
+    customerForm.style.display = 'none';
+    storeForm.style.display = 'none';
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.display = "flex";
     }
 }
