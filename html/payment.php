42b27f3277bdbb3a913dfc83fef6c780f85442fc
victorjomnipre <soritavictorjom@gmail.com>
2024-12-02 23:50:52 +0800

    "payment design"

--- html/payment.php
+++ html/payment.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel = "stylesheet" href = "css/payment.css">
+    <link rel="stylesheet" href="css/payment.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment</title>
 </head>
 <body>
-    <div id = "container">
-        <?php
-            $_SESSION['subTotal'] = $subTotal;  
-            $_SESSION['foodtaWalletBal'] = $balance->foodtawallet; 
-            echo "Payment = $subTotal";
-            echo "Balance = $balance->foodtawallet";
-        ?>
-    </div>
-    <?php
-        echo "<form action='index.php?command=processPayment' method = 'post'>";
-            echo "Choose a payment method";
-            echo "<select id='options' name='options'>";
-                echo "<option value='Cash'>Cash</option>";
-                echo "<option value='GCash'>GCash</option>";
-                echo "<option value='Card'>Card</option>";
-                echo "<option value='FoodtaWallet'>FoodtaWallet</option>";
-            echo "</select>";
-            echo "<input type='submit' value = 'Select'>";
-        echo "</form>";
-    ?>
+    <section class="paymentInfo">
+        <img class="ftSecLogo" src="images/foodTaSectionLogo(Green).png" alt="Food Ta Logo">
+        <h2>Pay Your Order</h2>
+        <div id="container">
+            <div class="payment-info">
+                <?php
+                    $_SESSION['subTotal'] = $subTotal;  
+                    $_SESSION['foodtaWalletBal'] = $balance->foodtawallet; 
+                    echo "<div class='payment-line'><span>Payment</span><span class='line'></span>$subTotal</span></div>";
+                    echo "<div class='payment-line'><span>Balance</span><span class='line'></span>$balance->foodtawallet</span></div>";
+                ?>
+            </div>
+            <hr>
+            <form action="index.php?command=processPayment" method="post" class="payment-form">
+                <label for="options">Choose a payment method:</label>
+                <select id="options" name="options">
+                    <option value="Cash">COD</option>
+                    <option value="GCash">GCash</option>
+                    <option value="Card">Card</option>
+                    <option value="FoodtaWallet">Food Ta! E-Wallet</option>
+                </select>
+                <input type="submit" value="Proceed Payment">
+            </form>
+        </div>
+    </section>
 </body>
 </html>
